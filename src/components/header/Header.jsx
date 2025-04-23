4068d4c88902120d1e70482a263f87b15fc6ef36
David Bogomolov <david.bogomolov2@gmail.com>
2025-04-23 19:10:45 +0300

    feat: added burger menu, fixed spacing

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom';
 import {WebRoutes} from '../../constants/routes';
 import SearchInput from "../search-input/SearchInput";
+import BurgerMenu from "../burger-menu/BurgerMenu";
 
 const Header = () => {
     return (
@@ -13,9 +14,10 @@ const Header = () => {
                 <Link to={WebRoutes.MENU} className={'mx-4 hover:text-secondary'}>Menu</Link>
                 <Link to={WebRoutes.ABOUT} className={'mx-4 hover:text-secondary'}>About</Link>
             </div>
-            <div className={'relative w-[150px] h-[32px]'}>
+            <div className={'hidden md:block relative w-[150px] h-[32px]'}>
                 <SearchInput/>
             </div>
+            <BurgerMenu/>
         </header>
     );
 };
